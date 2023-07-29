18028534c9b0a07e1cb1da181e25f0725edce4ec
yu-k-m <77780624+youkm1@users.noreply.github.com>
2023-07-30 03:22:45 +0900

    0730

--- app.py
+++ app.py
@@ -1,34 +1,17 @@
-from flask import Flask, redirect, url_for, request,render_template,session
-import torch
-import transformers #import BartTokenizer, BartForMaskedLM
+from flask import Flask, render_template,request
+import modeling
+
+app = Flask(__name__)
 
 
-app=Flask(__name__)
-
-#model
-model_name = 'digit82/kobart-summarization'
-#"bert-base-multilingual-cased"
-tokenizer = PreTrainedTokenizerFast.from_pretrained(model_name)
-model = BartForConditionalGeneration.from_pretrained(model_name)
-#tokenizer=BertTokenizer.from_pretrained(model_name)
-#model = BertForMaskedLM.from_pretrained(model_name)
-
-#요약 기능 함수
-def summary_txt(text):
-    inputs = tokenizer.encode(text, return_tensors="pt", add_special_tokens=True)
-    with torch.no_grad():
-        outputs = model.generate(inputs, max_length=100, num_beams=5, early_stopping=True)
-    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return summary
-
-@app.route("/", methods=['GET','POST'])
-def index():
-    summary = None
+@app.route('/', methods=('GET','POST'))
+def main():  # put application's code here
+    data = None
     if request.method == 'POST':
         text = request.form["text"]
         if text:
-            summary = summary_txt(text)
-    return render_template("index.html", summary=summary)
+            data = modeling.summarize_text(text)
+    return render_template("index.html", data=data)
 
-if __name__=='__main__':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
